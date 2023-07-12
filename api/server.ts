4351d4c43e0f14a62e099fae532a3a2a72f10299
MaxKeable <max.keable488@gmail.com>
2023-07-13 08:00:49 +1000

    test

--- api/server.ts
+++ api/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
-dotenv.config();
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
