b44e0f0761d66c97b1ac8efa4cadb8421b995c23
MaxKeable <max.keable488@gmail.com>
2023-07-13 08:31:56 +1000

    front end

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import path from "path";
+import { Request, Response } from "express";
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.use(express.static(path.join(__dirname, "../../client/build")));
+
+if (process.env.NODE_ENV === "production") {
+
+  app.get("*", (req: Request, res: Response) => {
+
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+
+  });
+
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
